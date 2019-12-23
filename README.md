8f4ceea5b84169ed44d4c6ff2207d80300118bfd
Benito Galvez <mystwalker131@gmail.com>
2019-12-23 22:59:42 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+clear
 # Badges and Schedules
 
 ## Objectives
--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+
+
+ def badge_maker(names) 
+   
+   return "Hello, my name is #{name}." 
+  end
+  
+  def batch_badge_creator(attendees)
+   
+    counter = 0
+   
+   badges.each do |badge|
+     puts "Hello, #{counter+1}! You'll be assigned to room #{badge}!" 
+     counter += 1 
+     end 
+ # badges
+
+end
+   # names =[ "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
+# def assign_rooms(speakers)
+
+
+# end
+  
+  
+  
